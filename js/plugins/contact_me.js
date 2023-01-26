635b6cbaa3df59a9776ea43cdeb147e1dd60efdb
Baptiste Mohamed <baptiste.mohamedvignal@gmail.com>
2023-01-26 19:14:04 +0100

    b

--- js/plugins/contact_me.js
+++ js/plugins/contact_me.js
@@ -35,7 +35,7 @@ $(function () {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("Your message has been sent.");
+                        .append("Votre message m'a bien été envoyé ! Merci !.");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -47,7 +47,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("Désolé " + firstName + ", Php n'est pas encore installé sur mon hebergeur (GitHub) ! ");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
